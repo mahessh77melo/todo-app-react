fc564b544cc3d3edf3d22e5a2988054ee132790b
mahessh77melo <magesh.lbj23@gmail.com>
2020-09-22 13:41:32 +0530

    final commit

--- src/state.js
+++ src/state.js
@@ -18,28 +18,3 @@ export const state = [
 	},
 ];
 export const MyContext = React.createContext(state);
-
-/* const Family = () => {
-	return (
-		<MyContext.Consumer>
-			{(context) => (
-				<div className="div">
-					<h1>The name is {context.state.name}</h1>
-					<h2>Age: {context.state.age}</h2>
-				</div>
-			)}
-		</MyContext.Consumer>
-	);
-};
-
-const Person = () => {
-	const state = [
-		{
-			id: 1,
-			name: "kamada",
-			age: 17,
-		},
-	];
-	return state.map((person) => <Family />);
-}; */
-// export default Person;
